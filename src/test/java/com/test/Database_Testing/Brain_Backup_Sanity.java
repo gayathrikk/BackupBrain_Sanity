2eac9824197eae0a9e6bdb021b1bd9ab1effa3a3
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2026-03-13 12:31:00 +0530

    Update Brain_Backup_Sanity.java

--- src/test/java/com/test/Database_Testing/Brain_Backup_Sanity.java
+++ src/test/java/com/test/Database_Testing/Brain_Backup_Sanity.java
@@ -13,13 +13,13 @@ public class Brain_Backup_Sanity {
     String user = "hbp";
     String password = "hbpsgbclab@123";
 
-    String host1 = "pp6.humanbrain.in";
+    String host1 = "pp2.humanbrain.in";
     String host2 = "ap7.humanbrain.in";
 
     @Test
     @Parameters("brainNumber")
     public void verifyBrainBackupConsistency(String brainNumber) throws Exception {
-        String path1 = "/mnt/remote/analytics/" + brainNumber;
+        String path1 = "/lustre/data/store10PB/repos1/iitlab/humanbrain/analytics/" + brainNumber;
         String path2 = "/mnt/local/store/repos1/iitlab/humanbrain/analytics/" + brainNumber;
 
         System.out.println("\n📦 Comparing TOTAL storage and file count for brain: " + brainNumber);
@@ -115,6 +115,7 @@ public class Brain_Backup_Sanity {
         return result;
     }
 }
+
 
 
 
